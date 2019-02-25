7b19c8b7f32ec9409b22c7743ca55644f671d078
hojakejake <mtho@connect.ust.hk>
2019-02-25 17:05:14 +0800

    updated enviroment

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [{
+    name: 'fypRecSys',
+    script: 'app.py',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+	  HTTP_PORT: '80',
+	  HTTPS_PORT: '443',
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : 'FYPBACKEND.MOOO.COM',
+      ref  : 'origin/master',
+      repo : 'git@github.com:hojakejake/fypBackend.git',
+      path : '/home/root/production/fypBackend',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
